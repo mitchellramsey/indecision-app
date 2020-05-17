0fd4a57938b32d5109fe5e6a23e1db2680e56e90
Mitchell Ramsey <mitchellramsey1992@gmail.com>
2020-05-16 21:11:48 -0400

    updates to publish to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = (env) => {
   return {
     entry: "./src/app.js",
     output: {
-      path: path.join(__dirname, "public"),
+      path: path.join(__dirname, "public", "dist"),
       filename: "bundle.js",
     },
     module: {
@@ -24,6 +24,7 @@ module.exports = (env) => {
     devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
+      publicPath: "/dist/",
     },
   };
 };
