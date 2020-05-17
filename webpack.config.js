52838fc2b66621fdfb4623daffaceaca803383cc
Mitchell Ramsey <mitchellramsey1992@gmail.com>
2020-05-16 21:25:16 -0400

    prod editing

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = (env) => {
   const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
   return {
-    entry: "./src/app.js",
+    entry: ["babel-polyfill", "./src/app.js"],
     output: {
       path: path.join(__dirname, "public", "dist"),
       filename: "bundle.js",
@@ -17,10 +19,26 @@ module.exports = (env) => {
         },
         {
           test: /\.s?css$/,
-          use: ["style-loader", "css-loader", "sass-loader"],
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
         },
       ],
     },
+    plugins: [CSSExtract],
     devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
